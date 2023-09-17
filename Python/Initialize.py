d6b0daf6b20aae7d76d81892db16adbc4cb2a5a6
Shishir1729 <shishirr@iisc.ac.in>
2023-09-17 22:03:29 +0530

    Added a TODO list and added conditional probability

--- Python/Initialize.py
+++ Python/Initialize.py
@@ -6,13 +6,13 @@ class Probability:
             raise ValueError("The list of values cannot have duplicates")
         if list_of_probabilities == None and function == None:
             raise ValueError("You must give either a list of probabilities or a function")
-        if list_of_probabilities != None and function != None:
+        elif list_of_probabilities != None and function != None:
             if self.check_pf(list_of_values, list_of_probabilities, function):
                 self.values = list_of_values
                 self.probabilities = list_of_probabilities
                 self.function = function
 
-        if list_of_probabilities != None:
+        elif list_of_probabilities != None:
             if len(list_of_values) != len(list_of_probabilities):
                 raise ValueError("The list of values and the list of probabilities must be the same length")
             if self.check_p(list_of_probabilities):
@@ -21,7 +21,7 @@ class Probability:
                 self.function = lambda x: list_of_probabilities[list_of_values.index(x)]
             else:
                 raise ValueError("The probabilities must add up to 1 and be non-negative")
-        if function != None:
+        elif function != None:
             if self.check_f(list_of_values, function):
                 self.values = list_of_values
                 self.probabilities = [function(x) for x in list_of_values]
@@ -31,7 +31,7 @@ class Probability:
             
     
     def check_p(self, list_of_probabilities):
-        if sum(list_of_probabilities) != 1:
+        if abs(sum(list_of_probabilities) - 1) > 1e-6:
             return False
         for x in list_of_probabilities:
             if x < 0:
@@ -42,12 +42,12 @@ class Probability:
         for x in list_of_values:
             if function(x) < 0:
                 return False
-        if sum([function(x) for x in list_of_values]) != 1:
+        if abs(sum([function(x) for x in list_of_values]) - 1) > 1e-6:
             return False
         return True
     
     def check_pf(self, list_of_values, list_of_probabilities, function):
-        if sum(list_of_probabilities) != 1:
+        if abs(sum(list_of_probabilities) - 1) > 1e-6:
             return False
         for x in list_of_probabilities:
             if x < 0:
@@ -57,9 +57,51 @@ class Probability:
                 return False
         return True
     
+    def check_subset(self, subset):
+        for x in subset:
+            if x not in self.values:
+                return False
+        return True
     
-    
+    def check_invariant(self, Invariant):
+        for x in self.values:
+            if not Invariant(x):
+                return False
+        return True
+
+    def Event(self, subset = None, Invariant = None):
+        if subset == None and Invariant == None:
+            raise ValueError("You must give either a subset or an invariant")
+        if subset != None and Invariant != None:
+            raise ValueError("You must give either a subset or an invariant, not both")
+        if subset != None:
+            if self.check_subset(subset):
+                return sum([self.probabilities[(self.values).index(i)] for i in subset])
+            else:
+                raise ValueError("The subset must be a subset of the values")
+        if Invariant != None:
+            if self.check_invariant(Invariant):
+                return sum([function(i) for i in self.values if Invariant(i)])
+            else:
+                raise ValueError("The invariant must be a function that takes a value and returns a boolean")
 
     
+    def conditioning(self, subset = None, Invariant = None):
+        if subset == None and Invariant == None:
+            raise ValueError("You must give either a subset or an invariant")
+        if subset != None and Invariant != None:
+            raise ValueError("You must give either a subset or an invariant, not both")
+        if subset != None:
+            if self.check_subset(subset):
+                return Probability(subset, [self.function(i)/self.Event(subset) for i in subset])
+            else:
+                raise ValueError("The subset must be a subset of the values")
+        if Invariant != None:
+            if self.check_invariant(Invariant):
+                return Probability(subset, [self.function(i)/self.Event(Invariant = Invariant) for i in subset])
+            else:
+                raise ValueError("The invariant must be a function that takes a value and returns a boolean")
 
 
+
+            
